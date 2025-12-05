a8e40702b6397024837ff9d1e48a7d6d65f875ed
petrienkoA4351 <annarew29@gmail.com>
2025-12-05 16:58:55 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-'''import java.io.BufferedReader;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -198,3 +198,4 @@ class SearchItems {
         }
     }
 }
+
